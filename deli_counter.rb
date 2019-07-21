6cb289311ffcba552ee4207aa6682fced96a696c
Colleen Halpenny <cp.halpenny@gmail.com>
2019-07-21 05:06:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
 def take_a_number (katz_deli, string)
   katz_deli << "#{string}"
-  puts "Welcome, #{stringp}. You are number #{index +1} in line."
+  puts "Welcome, #{string}. You are number #{index +1} in line."
 end
 
 
